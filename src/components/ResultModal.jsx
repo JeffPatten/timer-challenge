66da144744ba14be4fa7d32eb910d831e16c167b
Jeff Patten <jeffspatten@gmail.com>
2024-05-14 16:50:36 -0500

    updated components to use interval and to reset time once the modal is closed

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
 
   const dialog = useRef()
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -13,15 +16,15 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
   })
 
   return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
       <p>
-        The target time was <strong>{targetTime} seconds.</strong>
+        The target time was <strong>{targetTime} second{targetTime > 1 ? "s" : ""}.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
